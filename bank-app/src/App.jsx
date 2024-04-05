cb16de87cf38b49a1090c5a52a4782198bbd7751
Ademola Afolabi <afolabiademola27@gmail.com>
2024-04-05 13:56:25 +0100

    first commit

---
+++ bank-app/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p className="text-3xl font-bold underline">HEYYO</p>
+        <h1 className='.bg-blue-50 text-underline'>IM WORKING</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
