2192d792afae2391a8ecf1ec01ae2931798accee
Ademola Afolabi <afolabiademola27@gmail.com>
2024-06-20 16:49:51 +0100

    Implement Sending an Email When User Signs Up

--- node/Controllers/user.Controller.js
+++ node/Controllers/user.Controller.js
@@ -1,21 +1,22 @@
-const userModel = require("../Models/user.model")
-const jwt = require("jsonwebtoken")
-const bcrypt = require("bcrypt")
-require("dotenv").config()
-let secret = process.env.SECRET
+const userModel = require("../Models/user.model");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+let secret = process.env.SECRET;
 
 const cloudinary = require("cloudinary");
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const welcomeUser = (req, res) => {
-    res.send("Welcome to the home page")
-    console.log('Welcome to the home page');
-}
+  res.send("Welcome to the home page");
+  console.log("Welcome to the home page");
+};
 
 // const registerUser = async(req, res) => {
 //     // res.send("Welcome to the register user page");
@@ -66,7 +67,7 @@ const welcomeUser = (req, res) => {
 //         res.status(403).json({
 //             Message: 'Invalid credentials'
 //         });
-//     } else { 
+//     } else {
 //         const token = jwt.sign( {id: user._id}, secret, {expiresIn: "4h"})
 //         res.status(200).json({
 //             Message: 'Login successful',
@@ -77,109 +78,194 @@ const welcomeUser = (req, res) => {
 //     }
 // }
 
-const registerUser = async(req, res) => {
-    const {firstName, lastName, email, password} = req.body;
+// Create a transporter object using SMTP transport
 
+// Function to send an email
+// const sendEmail = async( to, subject, text ) => {
+//     const mailOptions = {
+//         from: process.env.USER_EMAIL, // Sender address
+//         to, // List of receivers
+//         subject,  // Subject line
+//         text, // Plain text body
+//     };
 
+//     try {
+//         // Send mail with defined transport object
+//         await transporter.sendMail(mailOptions);
+//         console.log('Email sent successfully');
+//     } catch (error) {
+//         console.error("Error sending email:", error);
+//     }
+// };
+
+const sendMail = async (email, firstName) => {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.USER_EMAIL, // Dev's Email address
+        pass: process.env.USER_PASS, // Dev's App Password
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+  
+    let emailText = `Hello ${firstName},\n\nWe're glad to have you onboard.\n\nBest,\nThe NairaNest Team`;
+  
+    const mailOptions = {
+      from: process.env.USER_EMAIL, // Sender address
+      to: email, // List of receivers
+      subject: "Welcome to NairaNest!", // Subject line
+      text: emailText, // Plain text body
+    };
+  
     try {
-
-        // Check if user already exists
-        const existingUser = await userModel.findOne({ email });
-
-        if (existingUser) {
-            return res.status(409).json({ message: "User already exists" });
-        }
-
-        // Hash password
-        const saltRound = 10;
-        const hashedPassword = bcrypt.hashSync(password, saltRound);
-
-        // Create new user
-        const newUser = new userModel({ firstName, lastName, email, password: hashedPassword });
-        
-        // Save new user
-        const result = await newUser.save();
-        console.log("User signed up and saved successfully");
-        console.log(result);
-        res.status(200).json({ message: "Registration successful", user: result });
-    } catch (error) {
-        console.error("Error during registration:", error);
-
-        // Handle unexpected errors
-        res.status(500).json({ message: "Registration failed. Please try again later.", error: error.message });
+      await transporter.sendMail(mailOptions);
+      console.log("Email sent successfully");
+    } catch (err) {
+      console.error("Error sending mail: " + err);
     }
-}
+  };
 
+const registerUser = async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  try {
+    // Check if user already exists
+    const existingUser = await userModel.findOne({ email });
+
+    if (existingUser) {
+      return res.status(409).json({ message: "User already exists" });
+    }
+
+    // Hash password
+    const saltRound = 10;
+    const hashedPassword = bcrypt.hashSync(password, saltRound);
+
+    // Create new user
+    const newUser = new userModel({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
+
+    // Save new user
+    const result = await newUser.save();
+    console.log("User signed up and saved successfully");
+
+    // Send a welcome email to the user
+    await sendMail(email, firstName);
+
+    console.log(result);
+    res.status(200).json({ message: "Registration successful", user: result });
+    
+  } catch (error) {
+    console.error("Error during registration:", error);
+
+    // Handle unexpected errors
+    res
+      .status(500)
+      .json({
+        message: "Registration failed. Please try again later.",
+        error: error.message,
+      });
+  }
+};
 
 const loginAttempts = {}; // Keep track of login attempts by IP address
 
 const loginUser = async (req, res) => {
-    const { email, password } = req.body;
-    const ip = req.ip;
+  const { email, password } = req.body;
+  const ip = req.ip;
 
-    const key = `${email}:${ip}`;
+  const key = `${email}:${ip}`;
 
-    if (!loginAttempts[key]) {
-        loginAttempts[key] = { count: 0, lastAttempt: Date.now() };
+  if (!loginAttempts[key]) {
+    loginAttempts[key] = { count: 0, lastAttempt: Date.now() };
+  }
+
+  const attemptsLimit = 5;
+  const lockoutTime = 300000;
+  const warningLimit = 3;
+
+  if (
+    loginAttempts[key].count >= attemptsLimit &&
+    Date.now() - loginAttempts[key].lastAttempt < lockoutTime
+  ) {
+    // 5 minutes lockout
+    const retryAfter =
+      lockoutTime - (Date.now() - loginAttempts[key].lastAttempt);
+    return res
+      .status(429)
+      .json({
+        message: "Too many login attempts. Please try again later.",
+        retryAfter,
+      });
+  }
+
+  try {
+    // Find user by email
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      loginAttempts[key].count += 1;
+      loginAttempts[key].lastAttempt = Date.now();
+      let warningMessage = "";
+      if (
+        loginAttempts[key].count >= warningLimit &&
+        loginAttempts[key].count < attemptsLimit
+      ) {
+        warningMessage = `Warning: You have ${
+          attemptsLimit - loginAttempts[key].count
+        } attempts left before being locked out.`;
+      }
+      return res
+        .status(404)
+        .json({ message: "User not found, sign up", warning: warningMessage });
     }
 
-    const attemptsLimit = 5;
-    const lockoutTime = 300000;
-    const warningLimit = 3;
+    // Check if password is correct
+    const correctPassword = await bcrypt.compare(password, user.password);
 
-    if (loginAttempts[key].count >= attemptsLimit && (Date.now() - loginAttempts[key].lastAttempt < lockoutTime)) { // 5 minutes lockout
-        const retryAfter = lockoutTime - (Date.now() - loginAttempts[key].lastAttempt)
-        return res.status(429).json({ message: 'Too many login attempts. Please try again later.', retryAfter });
+    if (!correctPassword) {
+      loginAttempts[key].count += 1;
+      loginAttempts[key].lastAttempt = Date.now();
+      let warningMessage = "";
+      if (
+        loginAttempts[key].count >= warningLimit &&
+        loginAttempts[key].count < attemptsLimit
+      ) {
+        warningMessage = `Warning: You have ${
+          attemptsLimit - loginAttempts[key].count
+        } attempts left before being locked out.`;
+      }
+      return res
+        .status(403)
+        .json({ message: "Invalid credentials", warning: warningMessage });
     }
 
-    try {
-        // Find user by email
-        const user = await userModel.findOne({ email });
+    // Reset login attempts on successful login
+    loginAttempts[key] = { count: 0, lastAttempt: Date.now() };
 
-        if (!user) {
-            loginAttempts[key].count += 1;
-            loginAttempts[key].lastAttempt = Date.now();
-            let warningMessage = ''
-            if (loginAttempts[key].count >= warningLimit && loginAttempts[key].count < attemptsLimit) {
-                warningMessage = `Warning: You have ${attemptsLimit - loginAttempts[key].count} attempts left before being locked out.`
-            }
-            return res.status(404).json({ message: 'User not found, sign up', warning: warningMessage });
-        }
+    // Generate JWT token
+    const token = jwt.sign({ id: user._id }, secret, { expiresIn: "30m" });
 
-        // Check if password is correct
-        const correctPassword = await bcrypt.compare(password, user.password);
-
-        if (!correctPassword) {
-            loginAttempts[key].count += 1;
-            loginAttempts[key].lastAttempt = Date.now();
-            let warningMessage = '';
-            if (loginAttempts[key].count >= warningLimit && loginAttempts[key].count < attemptsLimit) {
-                warningMessage = `Warning: You have ${attemptsLimit - loginAttempts[key].count} attempts left before being locked out.`;
-            }
-            return res.status(403).json({ message: 'Invalid credentials', warning: warningMessage });
-        }
-
-        // Reset login attempts on successful login
-        loginAttempts[key] = { count: 0, lastAttempt: Date.now() };
-
-        // Generate JWT token
-        const token = jwt.sign({ id: user._id }, secret, { expiresIn: "30m" });
-
-        // Send response with token and user details
-        res.status(200).json({
-            message: 'Login successful',
-            token: token,
-            user: user
-        });
-
-    } catch (error) {
-        // Handle error
-        console.error("Error:", error);
-        res.status(500).json({ message: "Internal server error" });
-    }
+    // Send response with token and user details
+    res.status(200).json({
+      message: "Login successful",
+      token: token,
+      user: user,
+    });
+  } catch (error) {
+    // Handle error
+    console.error("Error:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
-
-
 
 // const dashboard = async(req, res) => {
 //     // res.send("Welcome to the dashboard user page");
@@ -198,34 +284,37 @@ const loginUser = async (req, res) => {
 // }
 
 const dashboard = async (req, res) => {
-    try {
-        // Extract the token from the request headers
-        const token = req.headers.authorization.split(" ")[1];
-        
-        // Verify the token
-        const decoded = jwt.verify(token, secret);
-        
-        // If token verification is successful, get the user ID from the decoded token
-        const userId = decoded.id;
-        
-        // Find the user by ID in the database
-        const user = await userModel.findById(userId);
-        
-        // If user is not found, send an error response
-        if (!user) {
-            return res.status(404).json({ error: "User not found" });
-        }
-        
-        // If user is found, send the user details in the response
-        res.status(200).json({ user: user });
-    } catch (error) {
-        // If an error occurs, send an error response
-        console.error("Error:", error);
-        res.status(500).json({ error: "Internal server error" });
+  try {
+    // Extract the token from the request headers
+    const token = req.headers.authorization.split(" ")[1];
+
+    // Verify the token
+    const decoded = jwt.verify(token, secret);
+
+    // If token verification is successful, get the user ID from the decoded token
+    const userId = decoded.id;
+
+    // Find the user by ID in the database
+    const user = await userModel.findById(userId);
+
+    // If user is not found, send an error response
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
     }
+
+    // If user is found, send the user details in the response
+    res.status(200).json({ user: user });
+  } catch (error) {
+    // If an error occurs, send an error response
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
-
-
-
-module.exports = {welcomeUser, registerUser, loginUser, dashboard}
+module.exports = {
+  welcomeUser,
+  registerUser,
+  loginUser,
+  dashboard,
+  sendMail,
+};
--- node/Routes/user.Route.js
+++ node/Routes/user.Route.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const router = express.Router()
-const {welcomeUser, registerUser, loginUser, dashboard} = require("../Controllers/user.Controller")
+const {welcomeUser, registerUser, loginUser, dashboard, sendMail} = require("../Controllers/user.Controller")
 
 router.get("/user", welcomeUser);
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/sendmail", sendMail);
 router.get("/dashboard", dashboard);
 
 module.exports = router;
